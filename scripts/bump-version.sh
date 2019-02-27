b152f8c4bfa1e08df74a9b9bdd72c3fe075a2409
Andy Hunt <andy.p.hunt91@gmail.com>
2019-02-27 19:18:27 +0000

    Includes timestamp in version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-date +%Y-%m-%d > version.txt
+date +%Y%m%d%H%M%S > version.txt
 
 BRANCH=$(git symbolic-ref --short -q HEAD)
-git add version.txt
-git commit -m \"bump version to $(cat version.txt)\"
-git push origin $BRANCH
+git add "version.txt"
+git commit -m "bump version to $(cat version.txt)"
