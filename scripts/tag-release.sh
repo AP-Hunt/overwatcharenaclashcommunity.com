2018d03d5421975487a4f95c48dccf7a1e94bacb
Andy Hunt <andy.p.hunt91@gmail.com>
2019-03-01 20:20:40 +0000

    Corrects secrets and env var usage

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -2,10 +2,21 @@ git config credential.helper 'cache --timeout=120'
 git config user.email "email"
 git config user.name "CI Tagging"
 
-if git diff-tree --no-commit-id --name-only -r $CIRCLE_SHA1 | grep -q "version.txt"; then
+mkdir -p ~/.ssh/
+touch ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+echo $DEPLOY_SSH_PRIVATE_KEY > ~/.ssh/id_rsa
+
+ls -la ~/.ssh/
+
+touch ~/.ssh/known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+if git diff-tree --no-commit-id --name-only -r $DRONE_COMMIT_SHA | grep -q "version.txt"; then
     git tag -a "release-version-$(cat version.txt)" -m "$(cat version.txt)"
-    git push -q https://${GITHUB_TOKEN}@github.com/AP-Hunt/overwatcharenaclashcommunity.com.git "release-version-$(cat version.txt)"
+    git push "git@github.com:AP-Hunt/overwatcharenaclashcommunity.git" "release-version-$(cat version.txt)"
 else 
-    git tag -a "release-$CIRCLE_SHA1" -m  "$CIRCLE_SHA1"
-    git push -q https://${GITHUB_TOKEN}@github.com/AP-Hunt/overwatcharenaclashcommunity.com.git "release-$CIRCLE_SHA1"
+    git tag -a "release-$DRONE_COMMIT_SHA" -m  "$DRONE_COMMIT_SHA"
+    git push "git@github.com:AP-Hunt/overwatcharenaclashcommunity.git" "release-$DRONE_COMMIT_SHA"
 fi
